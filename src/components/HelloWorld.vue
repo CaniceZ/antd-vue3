c157db0e2c58851df90c374d2d55da0cf6d1de3d
zhengchuangjun <349373391@qq.com>
2022-04-29 15:12:28 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+import type { CascaderProps } from 'ant-design-vue';
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+const value = reactive([])
+const options2: CascaderProps['options'] = [
+  {
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [
+      {
+        value: 'hangzhou',
+        label: 'Hangzhou',
+        children: [
+          {
+            value: 'xihu',
+            label: 'West Lake',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [
+      {
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [
+          {
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+          },
+        ],
+      },
+    ],
+  },
+];
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <a-cascader v-model:value="value" :options="options2" placeholder="Please select" />
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
