f72958faedd573588ccbe358539776fe12235f2c
zhengchuangjun <349373391@qq.com>
2022-06-14 13:50:03 +0800

    增加http测试页面

--- src/views/system/System.vue
+++ src/views/system/System.vue
@@ -1,10 +1,14 @@
 <template>
-    <div>system</div>
+    <h2>接口返回数据：</h2>
+    <div>{{text}}</div>
 </template>
 <script setup lang="ts">
-import {onMounted} from "vue"
+import {onMounted, ref} from "vue"
 import { creditmanagementPage } from "@/api/home"
+const text = ref<object>({})
 onMounted(()=>{
-  creditmanagementPage({qq:"15919689346",password: "c8508fee37755bfbf682d0ff9034ac2c"})
+  creditmanagementPage({qq:"15919689346",password: "c8508fee37755bfbf682d0ff9034ac2c"}).then(res=>{
+    text.value = res.data
+  })
 })
 </script>
